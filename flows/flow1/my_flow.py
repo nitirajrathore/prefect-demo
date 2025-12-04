5ca13cf4819d7fc4d4a740ba44168ac4e42adc7d
Nitiraj Rathore <nitiraj.rathore@gmail.com>
2025-12-04 18:19:44 +0530

    flows/flow1: adding a flow inside a folder for testing if it works

---
+++ flows/flow1/my_flow.py
@@ -0,0 +1,16 @@
+from prefect import flow, task
+import platform
+import sys
+
+@task
+def log_environment():
+    print(f"Running on host: {platform.node()}")
+    print(f"Python version: {sys.version}")
+
+@flow(log_prints=True)
+def simple_k8s_flow():
+    print("Hello from Kubernetes!")
+    log_environment()
+
+if __name__ == "__main__":
+    simple_k8s_flow()
